3f101b9add5c3048b79435bbc966af067dd62e30
joewadk <jkabir000@citymail.cuny.edu>
2024-11-11 13:18:42 -0500

    git init

---
+++ app.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+load_dotenv()
+import os
+load_dotenv()
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+audio_file = open("data/audio.m4a", "rb")
+transcription = client.audio.transcriptions.create(
+  model="whisper-1", 
+  file=audio_file, 
+  response_format="text"
+)
+
+#store in txt file (so i dont have to read a json file)
+with open("transcription.txt", "w") as file:
+    file.write(transcription)
