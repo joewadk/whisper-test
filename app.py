7916f6ef9e0c594040e01f05a12b172a001ce502
joewadk <jkabir000@citymail.cuny.edu>
2024-11-11 13:19:07 -0500

    removed redundancy

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from openai import OpenAI
 from dotenv import load_dotenv
 load_dotenv()
 import os
-load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 audio_file = open("data/audio.m4a", "rb")
